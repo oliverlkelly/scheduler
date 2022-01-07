e2587245fcec37967ad98f98051f84cc312207ea
oliverlkelly <18181648+oliverlkelly@users.noreply.github.com>
2022-01-08 00:16:59 +1100

    creating array of hours in 24hr time

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,2 +1,9 @@
-var day = moment();
-$("#currentDay").text(day.format('dddd, MMMM Do'))
+var momentVar = moment();
+$("#currentDay").text(momentVar.format('dddd, MMMM Do'));
+var currentTime = momentVar.hour();
+
+
+var fullDayHours = 
+    Array.from(new Array(24)).map((val, i) => {
+        return moment().hour(i).format('HH:00');
+    });
