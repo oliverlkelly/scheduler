73567a8e596abb48c27ec90651221071faae74ee
oliverlkelly <18181648+oliverlkelly@users.noreply.github.com>
2022-01-10 01:18:26 +1100

    fixed css issues I was having, added function to change text area colour

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,6 +2,20 @@ var momentVar = moment();
 $("#currentDay").text(momentVar.format('dddd, MMMM Do'));
 var currentTime = momentVar.hour();
 
+function timeIndicator(elements){
+    elements.forEach(t =>{
+        if(t.dataset.time === currentTime){
+            t.classList.add("present");
+        }
+        else if(t.dataset.time < currentTime){
+            t.classList.add("past");
+        }
+        else{
+            t.classList.add("future");
+        }
+    })
+}
+
 
 var fullDayHours = 
     Array.from(new Array(24)).map((val, i) => {
@@ -9,9 +23,9 @@ var fullDayHours =
     });
 
 fullDayHours.forEach((i) => {
-    var timetableBlock = $(`<form data-name=${i} class=row></form>`);
-    var blockTime = $(`<div class="hour time-block">${i}</div>`);
-    var entryField = $(`<textarea name=${i} class=textarea></textarea>`);
+    var timetableBlock = $(`<form data-time=${i} class=row></form>`);
+    var blockTime = $(`<div class="hour"><p class="hourText">${i}</p></div>`);
+    var entryField = $(`<textarea time=${i} class=textarea></textarea>`);
     var saveButton = $(`<button class=saveBtn><i class="fas fa-save"></i></button>`);
     timetableBlock.append(blockTime);
     timetableBlock.append(entryField);
